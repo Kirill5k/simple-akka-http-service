2f54f7f6a3f6d55d7507439ed57ee731dc98c625
kirill <immotional@aol.com>
2019-10-29 15:35:46 +0000

    defined messages for ticketseller

--- src/main/scala/io/kirill/boxofficeapp/events/Event.scala
+++ src/main/scala/io/kirill/boxofficeapp/events/Event.scala
@@ -2,6 +2,6 @@ package io.kirill.boxofficeapp.events
 
 import com.github.nscala_time.time.Imports._
 
-case class Ticket(id: String, eventName: String)
+case class Ticket(id: String, eventName: String, purchaseDate: DateTime)
 
 case class Event(name: String, location: String, date: DateTime, seatsCount: Int)
--- src/main/scala/io/kirill/boxofficeapp/events/TicketsSeller.scala
+++ src/main/scala/io/kirill/boxofficeapp/events/TicketsSeller.scala
@@ -3,9 +3,18 @@ package io.kirill.boxofficeapp.events
 import akka.actor.{Actor, ActorLogging, Props}
 
 object TicketsSeller {
+  case object GetEvent
+  case object GetTickets
+  case class ReserveTickets(count: Int)
+
   def props(event: Event) = Props(new TicketsSeller(event))
 }
 
 class TicketsSeller (event: Event) extends Actor with ActorLogging {
-  override def receive: Receive = ???
+
+  override def receive: Receive = withTickets(List())
+
+  def withTickets(tickets: Seq[Ticket]): Receive = {
+    case message => log.info(s"received message: ${message}")
+  }
 }
