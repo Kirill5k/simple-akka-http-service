a501120ecd59af831b3f8747d43ed54cdb8ff09c
kirill <immotional@aol.com>
2019-10-29 15:10:11 +0000

    fixed compilation errors

--- src/main/scala/io/kirill/boxofficeapp/events/EventsApi.scala
+++ src/main/scala/io/kirill/boxofficeapp/events/EventsApi.scala
@@ -2,6 +2,7 @@ package io.kirill.boxofficeapp.events
 
 import akka.actor.{ActorRef, ActorSystem}
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
+import akka.http.scaladsl.model.StatusCodes
 import akka.pattern.ask
 import akka.util.Timeout
 import akka.http.scaladsl.server.Directives._
@@ -20,29 +21,35 @@ trait EventsApiJsonSupport extends DefaultJsonProtocol with SprayJsonSupport {
 }
 
 class EventsApi private (eventsManager: ActorRef, timeout: Timeout) extends EventsApiJsonSupport {
+  import EventsManager._
 
   val eventsRoute = pathPrefix("events") {
     path(Segment) { eventName =>
       path("tickets") {
         put {
           // buy tickets for event
+          complete(StatusCodes.ServiceUnavailable)
         }
       } ~
       pathEndOrSingleSlash {
         get {
           // get event by name
+          complete(StatusCodes.ServiceUnavailable)
         } ~
         delete {
           // cancel event by name
+          complete(StatusCodes.ServiceUnavailable)
         }
       }
     } ~
     pathEndOrSingleSlash {
       get {
         // get all events
-      }~
+        complete(StatusCodes.ServiceUnavailable)
+      } ~
       post {
         // create new event
+        complete(StatusCodes.ServiceUnavailable)
       }
     }
   }
